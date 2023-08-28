bfadc25383a71517fcf8d914cf56bbcd82f87536
Valeria Betancourt <vbhdz98@live.com.mx>
2023-08-28 13:16:42 -0600

    add model horses

---
+++ db/migrate/20230828191101_create_horses.rb
@@ -0,0 +1,15 @@
+class CreateHorses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :horses do |t|
+      t.string :category
+      t.string :breed
+      t.decimal :height
+      t.string :pedigree
+      t.text :accomplishments
+      t.string :price
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
